32bc712daa5ea3b6d5649ece0a6fadca4894b148
abdulrahman <abdulrahmanmahfudh@gmail.com>
2020-05-18 14:18:52 +0300

    Initial commit

---
+++ app/src/main/java/com/globomed/learn/DatabaseHelper.kt
@@ -0,0 +1,30 @@
+package com.globomed.learn
+
+import android.content.Context
+import android.database.sqlite.SQLiteDatabase
+import android.database.sqlite.SQLiteOpenHelper
+
+class DatabaseHelper(context: Context?
+
+) : SQLiteOpenHelper(context, DATABASE_NAME, null, DATABASE_VERSION) {
+    override fun onCreate(db: SQLiteDatabase?) {
+        db?.execSQL(GloboMedDBContract.EmployeeEntry.SQL_CREATE_ENTRIES)
+    }
+
+    override fun onUpgrade(db: SQLiteDatabase?, oldVersion: Int, newVersion: Int) {
+        db?.execSQL(GloboMedDBContract.EmployeeEntry.SQL_DROP_TABLE)
+    }
+
+    override fun onDowngrade(db: SQLiteDatabase?, oldVersion: Int, newVersion: Int) {
+        super.onDowngrade(db, oldVersion, newVersion)
+    }
+
+    override fun onOpen(db: SQLiteDatabase?) {
+        super.onOpen(db)
+    }
+
+    companion object{
+        const val DATABASE_NAME = "globomed.db"
+        const val DATABASE_VERSION = 1
+    }
+}
